879eeb0d5fe43ca9f72a7c6a6ff5ba1f88a311ab
zaichao pan <zaichaopan@gmail.com>
2020-03-04 23:00:56 -0800

    Upgrade js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import _ from 'lodash';
 import loadProgressBar from './helpers/progress-bar';
 
-window._ = _;
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
